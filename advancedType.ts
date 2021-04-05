d4ed6673977d5967cc629c52dab2047408528fee
bmzhanglei <115097376@qq.com>
2021-04-05 22:12:03 +0800

    m

--- advancedType.ts
+++ advancedType.ts
@@ -59,3 +59,71 @@ function myProxify<T>(o: T): Proxify<T> {
 
 let originalProps = unproxify<Student>(annyProxify);
 console.log(originalProps)
+
+class StudentInfo{}
+type t10 = InstanceType<typeof StudentInfo>
+
+//Partial, Readonly, Record, Pick
+/**
+ * Make all properties in T optional
+ */
+//  type Partial<T> = {
+//     [P in keyof T]?: T[P];
+// };
+
+/**
+ * Make all properties in T readonly
+ */
+// type Readonly<T> = {
+//     readonly [P in keyof T]: T[P];
+// };
+
+/**
+ * From T, pick a set of properties whose keys are in the union K
+ */
+// type Pick<T, K extends keyof T> = {
+//     [P in K]: T[P];
+// };
+
+/**
+ * Construct a type with a set of properties K of type T
+ */
+// type Record<K extends keyof any, T> = {
+//     [P in K]: T;
+// };
+
+type Coord = Record<'x' | 'y', number>;
+
+// 等同于
+// type Coord = {
+// 	x: number;
+// 	y: number;
+// }
+
+// type Coord = Partial<Record<'x' | 'y', number>>;
+
+// // 等同于
+// type Coord = {
+// 	x?: number;
+// 	y?: number;
+// }
+
+// type Coord = Readonly<Record<'x' | 'y', number>>;
+
+// 等同于
+// type Coord = {
+//     readonly x: number;
+//     readonly y: number;
+// }
+
+// 如果进行了修改，则会报错：
+// const c: Coord = { x: 1, y: 1 };
+// c.x = 2; // Error: Cannot assign to 'x' because it is a read-only property.
+
+// type Coord = Record<'x' | 'y', number>;
+// type CoordX = Pick<Coord, 'x'>;
+
+// 等用于
+// type CoordX = {
+// 	x: number;
+// }
